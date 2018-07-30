1c158f3e0590cdab48feb0166fa4802f579f8fff
idevol <saul.diaz.v@gmail.com>
2018-07-29 21:20:48 -0500

    BTQ Buffer

--- wp-content/mu-plugins/btq-buffer/btq-buffer.php
+++ wp-content/mu-plugins/btq-buffer/btq-buffer.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * Plugin Name: BTQ Buffer
+ * Plugin URI: http://btqdesign.com/plugins/btq-buffer/
+ * Description: Control de la salida del sitio WordPress
+ * Version: 1.0
+ * Author: BTQ Design
+ * Author URI: http://btqdesign.com/
+ * Requires at least: 4.9.7
+ * Tested up to: 4.9.7
+ *
+ * Text Domain: btq-buffer
+ * Domain Path: /languages
+ *
+ * @package btq-buffer
+ * @category Core
+ * @author BTQ Design
+ */
 
 /**
  * Output Buffering
@@ -11,20 +28,18 @@ ob_start();
 add_action(
 	'shutdown', 
 	function() {
-	    $final = '';
+		$final = '';
 	
-	    // We'll need to get the number of ob levels we're in, so that we can iterate over each, collecting
-	    // that buffer's output into the final output.
-	    $levels = ob_get_level();
-	
-	    for ($i = 0; $i < $levels; $i++)
-	    {
-	        $final .= ob_get_clean();
-	    }
-	
-	    // Apply any filters to the final output
+		// We'll need to get the number of ob levels we're in, so that we can iterate over each, collecting
+		// that buffer's output into the final output.
+		$levels = ob_get_level();
+		
+		for ($i = 0; $i < $levels; $i++) {
+			$final .= ob_get_clean();
+		}
+		// Apply any filters to the final output
 		echo apply_filters('final_output', $final);
-	}, 
+	},
 	0
 );
 
